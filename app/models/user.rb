6ed025df59681bd0298442c3e71fbd8643317522
Alina Moskieva <alina.moskieva@flatstack.com>
2018-03-29 15:51:24 +0300

    Remove conformition and recoverable modules

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable, :confirmable,
-    :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :rememberable, :trackable, :validatable
 
   validates :full_name, presence: true
 end
